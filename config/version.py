4aa0567f775ba0bd78c2f77a4b1da715ff72fac3
Luna the GSD <luna@huduglue.dev>
2026-01-14 14:50:17 +0000

    v2.24.4 - Fix random background images not displaying (added has-background CSS styling)

--- config/version.py
+++ config/version.py
@@ -2,11 +2,11 @@
 Version information for HuduGlue
 """
 
-VERSION = '2.24.3'
+VERSION = '2.24.4'
 VERSION_INFO = {
     'major': 2,
     'minor': 24,
-    'patch': 3,
+    'patch': 4,
     'status': 'stable',  # alpha, beta, rc, stable
 }
 
