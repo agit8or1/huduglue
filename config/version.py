96ba3ee07e16c60cb4781e0031c7c38b65d2626c
Luna the GSD <agit8or@agit8or.net>
2026-01-14 14:55:56 +0000

    v2.24.5 - Fix modal button click issues (z-index and pointer-events)

--- config/version.py
+++ config/version.py
@@ -2,11 +2,11 @@
 Version information for HuduGlue
 """
 
-VERSION = '2.24.4'
+VERSION = '2.24.5'
 VERSION_INFO = {
     'major': 2,
     'minor': 24,
-    'patch': 4,
+    'patch': 5,
     'status': 'stable',  # alpha, beta, rc, stable
 }
 
