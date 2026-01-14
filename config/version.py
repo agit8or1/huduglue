b345a8a245af48d39ee0f7a06bdac442297ab3d3
Luna the GSD <agit8or@agit8or.net>
2026-01-14 03:34:19 +0000

    v2.20.2: Staff permission fix and copy button enhancements

--- config/version.py
+++ config/version.py
@@ -2,11 +2,11 @@
 Version information for HuduGlue
 """
 
-VERSION = '2.20.1'
+VERSION = '2.20.2'
 VERSION_INFO = {
     'major': 2,
     'minor': 20,
-    'patch': 1,
+    'patch': 2,
     'status': 'stable',  # alpha, beta, rc, stable
 }
 
