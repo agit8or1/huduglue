4a2e5379a0737b8e9a2741df33a9ae902f5ea5af
Luna the GSD <luna@huduglue.dev>
2026-01-14 14:45:58 +0000

    v2.24.3 - Fix dropdown z-index issue preventing menus from appearing on top of content

--- config/version.py
+++ config/version.py
@@ -2,11 +2,11 @@
 Version information for HuduGlue
 """
 
-VERSION = '2.24.2'
+VERSION = '2.24.3'
 VERSION_INFO = {
     'major': 2,
     'minor': 24,
-    'patch': 2,
+    'patch': 3,
     'status': 'stable',  # alpha, beta, rc, stable
 }
 
