1899d691956e2d74bd51ddcbc791823a0bebdfd6
Luna the GSD <agit8or@agit8or.net>
2026-01-29 01:37:11 +0000

    v2.25.4 - Bump version

--- config/version.py
+++ config/version.py
@@ -2,11 +2,11 @@
 Version information for HuduGlue
 """
 
-VERSION = '2.25.3'
+VERSION = '2.25.4'
 VERSION_INFO = {
     'major': 2,
     'minor': 25,
-    'patch': 3,
+    'patch': 4,
     'status': 'stable',  # alpha, beta, rc, stable
 }
 
