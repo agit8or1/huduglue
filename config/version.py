ef58188180bd7c6d720cff34b4bfcb23f0aed0bb
Luna the GSD <agit8or@agit8or.net>
2026-02-12 03:02:05 +0000

    v2.84.2 - Final release with JavaScript fix

--- config/version.py
+++ config/version.py
@@ -2,11 +2,11 @@
 Version information for HuduGlue
 """
 
-VERSION = '2.84.1'
+VERSION = '2.84.2'
 VERSION_INFO = {
     'major': 2,
     'minor': 84,
-    'patch': 1,
+    'patch': 2,
     'status': 'stable',  # alpha, beta, rc, stable
 }
 
