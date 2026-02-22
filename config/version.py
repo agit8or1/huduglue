27cacfda5efedfc58d79b1c634a279b671be1721
Luna the GSD <agit8or@agit8or.net>
2026-02-22 00:42:00 +0000

    Bump to v3.11.5 - GUI updates fully automated

--- config/version.py
+++ config/version.py
@@ -2,11 +2,11 @@
 Version information for Client St0r
 """
 
-VERSION = '3.11.4'
+VERSION = '3.11.5'
 VERSION_INFO = {
     'major': 3,
     'minor': 11,
-    'patch': 4,
+    'patch': 5,
     'status': 'stable',  # alpha, beta, rc, stable
 }
 
